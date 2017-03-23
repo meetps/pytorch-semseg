c311bf004ecc486cf61d67e4b982052c6923a58a
meetshah1995 <meetshah1995@gmail.com>
2017-03-23 23:14:42 +0530

    Add dataLoader for camvid

---
+++ ptsemseg/loader/camvid_loader.py
@@ -0,0 +1,104 @@
+import os
+import collections
+import torch
+import torchvision
+import numpy as np
+import scipy.misc as m
+import matplotlib.pyplot as plt
+
+from torch.utils import data
+
+def default_loader(path):
+    return Image.open(path)
+
+class camvidLoader(data.Dataset):
+    def __init__(self, root, split="train", is_transform=False):
+        self.root = root
+        self.split = split
+        self.is_transform = is_transform
+        self.mean_bgr = np.array([104.00698793, 116.66876762, 122.67891434])
+        self.files = collections.defaultdict(list)
+
+        for split in ["train", "test", "val"]:
+            file_list = os.listdir(root + '/' + split)
+            self.files[split] = file_list
+
+    def __len__(self):
+        return len(self.files[self.split])
+
+    def __getitem__(self, index):
+        img_name = self.files[self.split][index]
+        img_path = self.root + '/' + self.split + '/' + img_name
+        lbl_path = self.root + '/' + self.split + 'annot/' + img_name
+
+        img = m.imread(img_path)
+        img = np.array(img, dtype=np.uint8)
+
+        lbl = m.imread(lbl_path)
+        lbl = np.array(lbl, dtype=np.int32)
+
+        if self.is_transform:
+            img, lbl = self.transform(img, lbl)
+
+        return img, lbl
+
+    def transform(self, img, lbl):
+        img = img[:, :, ::-1]
+        img = img.astype(np.float64)
+        img -= self.mean_bgr
+        img = img.transpose(2, 0, 1)
+
+        img = torch.from_numpy(img).float()
+        lbl = torch.from_numpy(lbl).long()
+        return img, lbl
+
+    def decode_segmap(self, temp, plot=False):
+        Sky = [128, 128, 128]
+        Building = [128, 0, 0]
+        Pole = [192, 192, 128]
+        Road_marking = [255, 69, 0]
+        Road = [128, 64, 128]
+        Pavement = [60, 40, 222]
+        Tree = [128, 128, 0]
+        SignSymbol = [192, 128, 128]
+        Fence = [64, 64, 128]
+        Car = [64, 0, 128]
+        Pedestrian = [64, 64, 0]
+        Bicyclist = [0, 128, 192]
+        Unlabelled = [0, 0, 0]
+
+        label_colours = np.array([Sky, Building, Pole, Road, Pavement,
+                                  Tree, SignSymbol, Fence, Car, Pedestrian,
+                                  Bicyclist, Unlabelled])
+        r = temp.copy()
+        g = temp.copy()
+        b = temp.copy()
+        for l in range(0, 11):
+            r[temp == l] = label_colours[l, 0]
+            g[temp == l] = label_colours[l, 1]
+            b[temp == l] = label_colours[l, 2]
+
+        rgb = np.zeros((temp.shape[0], temp.shape[1], 3))
+        rgb[:, :, 0] = (r/255.0)
+        rgb[:, :, 1] = (g/255.0)
+        rgb[:, :, 2] = (b/255.0)
+        if plot:
+            plt.imshow(rgb)
+            plt.show()
+        else:
+            return rgb
+
+if __name__ == '__main__':
+    local_path = '/home/meetshah1995/datasets/segnet/CamVid'
+    dst = camvidLoader(local_path, is_transform=True)
+    trainloader = data.DataLoader(dst, batch_size=4)
+    for i, data in enumerate(trainloader):
+        imgs, labels = data
+        if i == 0:
+            img = torchvision.utils.make_grid(imgs).numpy()
+            img = np.transpose(img, (1, 2, 0))
+            img = img[:, :, ::-1]
+            plt.imshow(img)
+            plt.show()
+            plt.imshow(dst.decode_segmap(labels.numpy()[i]))
+            plt.show()
