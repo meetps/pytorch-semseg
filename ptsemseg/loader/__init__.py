7ddece7e8ef2f5e4e42e9b4c7c5a496ebecc58f8
meetshah1995 <meetshah1995@gmail.com>
2017-07-04 11:53:27 +0300

    Add ADE20K dataloader

--- ptsemseg/loader/__init__.py
+++ ptsemseg/loader/__init__.py
@@ -2,11 +2,13 @@ import json
 
 from ptsemseg.loader.pascal_voc_loader import pascalVOCLoader
 from ptsemseg.loader.camvid_loader import camvidLoader
+from ptsemseg.loader.ade20k_loader import ADE20KLoader
 
 def get_loader(name):
     return {
         'pascal': pascalVOCLoader,
         'camvid': camvidLoader,
+        'ade20k': ADE20KLoader,
     }[name]
 
 def get_data_path(name):
