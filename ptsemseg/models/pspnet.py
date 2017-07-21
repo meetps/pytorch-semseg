edf63a0dcd142817051243759ac841e065b68961
Ishrat Badami <badami@vision.rwth-aachen.de>
2017-07-21 16:55:58 +0200

    Added support for multiple resnet

--- ptsemseg/models/pspnet.py
+++ ptsemseg/models/pspnet.py
@@ -2,18 +2,28 @@ import torch.nn as nn
 
 from utils import *
 
+Resnets = {'resnet18' :{'layers':[2, 2, 2, 2],'filters':[64, 128, 256, 512], 'block':residualBlock,'expansion':1},
+           'resnet34' :{'layers':[3, 4, 6, 3],'filters':[64, 128, 256, 512], 'block':residualBlock,'expansion':1},
+           'resnet50' :{'layers':[3, 4, 6, 3],'filters':[64, 128, 256, 512], 'block':residualBottleneck,'expansion':4},
+           'resnet101' :{'layers':[3, 4, 23, 3],'filters':[64, 128, 256, 512], 'block':residualBottleneck,'expansion':4},
+           'resnet152':{'layers':[3, 8, 36, 3],'filters':[64, 128, 256, 512], 'block':residualBottleneck,'expansion':4}
+            }
+
+
 class pspnet(nn.Module):
 
-    def __init__(self, feature_scale=4, n_classes=21, is_deconv=True, in_channels=3, is_batchnorm=True):
+    def __init__(self, resnet='resnet18', feature_scale=4, n_classes=21, is_deconv=True, in_channels=3, is_batchnorm=True):
         super(pspnet, self).__init__()
         self.is_deconv = is_deconv
         self.in_channels = in_channels
         self.is_batchnorm = is_batchnorm
         self.feature_scale = feature_scale
-        self.layers = [2, 2, 2, 2] # Currently hardcoded for ResNet-18
 
-        filters = [64, 128, 256, 512]
+        assert resnet in Resnets.keys(), 'Not a valid resnet, currently supported resnets are 18, 34, 50, 101 and 152'
+        layers = Resnets[resnet]['layers']
+        filters = Resnets[resnet]['filters']
         # filters = [x / self.feature_scale for x in filters]
+        expansion =Resnets[resnet]['expansion']
 
         self.inplanes = filters[0]
 
@@ -23,19 +33,19 @@ class pspnet(nn.Module):
                                                padding=3, stride=2, bias=False)
         self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
 
-        block = residualBlock
-        self.encoder1 = self._make_layer(block, filters[0], self.layers[0])
-        self.encoder2 = self._make_layer(block, filters[1], self.layers[1], stride=2)
-        self.encoder3 = self._make_layer(block, filters[2], self.layers[2], stride=2)
-        self.encoder4 = self._make_layer(block, filters[3], self.layers[3], stride=2)
+        block = Resnets[resnet]['block']
+        self.encoder1 = self._make_layer(block, filters[0], layers[0])
+        self.encoder2 = self._make_layer(block, filters[1], layers[1], stride=2)
+        self.encoder3 = self._make_layer(block, filters[2], layers[2], stride=2)
+        self.encoder4 = self._make_layer(block, filters[3], layers[3], stride=2)
         self.avgpool = nn.AvgPool2d(7)
 
 
         # Decoder
-        self.decoder4 = linknetUp(filters[3], filters[2])
-        self.decoder3 = linknetUp(filters[2], filters[1])
-        self.decoder2 = linknetUp(filters[1], filters[0])
-        self.decoder1 = linknetUp(filters[0], filters[0])
+        self.decoder4 = linknetUp(filters[3]*expansion, filters[2]*expansion)
+        self.decoder3 = linknetUp(filters[2]*expansion, filters[1]*expansion)
+        self.decoder2 = linknetUp(filters[1]*expansion, filters[0]*expansion)
+        self.decoder1 = linknetUp(filters[0]*expansion, filters[0])
 
         # Final Classifier
         self.finaldeconvbnrelu1 = deconv2DBatchNormRelu(filters[0], 32/feature_scale, 2, 2, 0)
@@ -78,4 +88,3 @@ class pspnet(nn.Module):
         f3 = self.finalconv3(f2)
 
         return f3
-
