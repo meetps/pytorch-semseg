50befd4768d98d5ef66b22fffe6941304183e0b9
Ishrat Badami <badami@vision.rwth-aachen.de>
2017-08-04 16:08:12 +0200

    Bug fixed for linknet, inserted multiple resent support

--- ptsemseg/models/linknet.py
+++ ptsemseg/models/linknet.py
@@ -2,18 +2,28 @@ import torch.nn as nn
 
 from utils import *
 
-class linknet(nn.Module):
+Resnets = {'resnet18' :{'layers':[2, 2, 2, 2],'filters':[64, 128, 256, 512], 'block':residualBlock,'expansion':1},
+           'resnet34' :{'layers':[3, 4, 6, 3],'filters':[64, 128, 256, 512], 'block':residualBlock,'expansion':1},
+           'resnet50' :{'layers':[3, 4, 6, 3],'filters':[64, 128, 256, 512], 'block':residualBottleneck,'expansion':4},
+           'resnet101' :{'layers':[3, 4, 23, 3],'filters':[64, 128, 256, 512], 'block':residualBottleneck,'expansion':4},
+           'resnet152':{'layers':[3, 8, 36, 3],'filters':[64, 128, 256, 512], 'block':residualBottleneck,'expansion':4}
+            }
 
-    def __init__(self, feature_scale=4, n_classes=21, is_deconv=True, in_channels=3, is_batchnorm=True):
-        super(linknet, self).__init__()
+
+class pspnet(nn.Module):
+
+    def __init__(self, resnet='resnet18', feature_scale=4, n_classes=21, is_deconv=True, in_channels=3, is_batchnorm=True):
+        super(pspnet, self).__init__()
         self.is_deconv = is_deconv
         self.in_channels = in_channels
         self.is_batchnorm = is_batchnorm
         self.feature_scale = feature_scale
-        self.layers = [2, 2, 2, 2] # Currently hardcoded for ResNet-18
 
-        filters = [64, 128, 256, 512]
-        filters = [x / self.feature_scale for x in filters]
+        assert resnet in Resnets.keys(), 'Not a valid resnet, currently supported resnets are 18, 34, 50, 101 and 152'
+        layers = Resnets[resnet]['layers']
+        filters = Resnets[resnet]['filters']
+        # filters = [x / self.feature_scale for x in filters]
+        expansion =Resnets[resnet]['expansion']
 
         self.inplanes = filters[0]
 
@@ -23,40 +33,35 @@ class linknet(nn.Module):
                                                padding=3, stride=2, bias=False)
         self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
 
-        block = residualBlock
-        self.encoder1 = self._make_layer(block, filters[0], self.layers[0])
-        self.encoder2 = self._make_layer(block, filters[1], self.layers[1], stride=2)
-        self.encoder3 = self._make_layer(block, filters[2], self.layers[2], stride=2)
-        self.encoder4 = self._make_layer(block, filters[3], self.layers[3], stride=2)
+        block = Resnets[resnet]['block']
+        self.encoder1 = self._make_layer(block, filters[0], layers[0])
+        self.encoder2 = self._make_layer(block, filters[1], layers[1], stride=2)
+        self.encoder3 = self._make_layer(block, filters[2], layers[2], stride=2)
+        self.encoder4 = self._make_layer(block, filters[3], layers[3], stride=2)
         self.avgpool = nn.AvgPool2d(7)
 
 
         # Decoder
-        self.decoder4 = linknetUp(filters[3], filters[2])
-        self.decoder4 = linknetUp(filters[2], filters[1])
-        self.decoder4 = linknetUp(filters[1], filters[0])
-        self.decoder4 = linknetUp(filters[0], filters[0])
+        self.decoder4 = linknetUp(filters[3]*expansion, filters[2]*expansion)
+        self.decoder3 = linknetUp(filters[2]*expansion, filters[1]*expansion)
+        self.decoder2 = linknetUp(filters[1]*expansion, filters[0]*expansion)
+        self.decoder1 = linknetUp(filters[0]*expansion, filters[0])
 
         # Final Classifier
-        self.finaldeconvbnrelu1 = nn.Sequential(nn.ConvTranspose2d(filters[0], 32/feature_scale, 3, 2, 1),
-                                      nn.BatchNorm2d(32/feature_scale),
-                                      nn.ReLU(inplace=True),)
+        self.finaldeconvbnrelu1 = deconv2DBatchNormRelu(filters[0], 32/feature_scale, 2, 2, 0)
         self.finalconvbnrelu2 = conv2DBatchNormRelu(in_channels=32/feature_scale, k_size=3, n_filters=32/feature_scale, padding=1, stride=1)
-        self.finalconv3 = nn.Conv2d(32/feature_scale, n_classes, 2, 2, 0)
+        self.finalconv3 = nn.Conv2d(int(32/feature_scale), int(n_classes), 3, 1, 1)
 
     def _make_layer(self, block, planes, blocks, stride=1):
         downsample = None
         if stride != 1 or self.inplanes != planes * block.expansion:
-            downsample = nn.Sequential(nn.Conv2d(self.inplanes, planes * block.expansion,
-                                                 kernel_size=1, stride=stride, bias=False),
-                                       nn.BatchNorm2d(planes * block.expansion),)
+            downsample = conv2DBatchNorm(self.inplanes, planes*block.expansion, k_size=1, stride=stride, padding=0, bias=False)
         layers = []
         layers.append(block(self.inplanes, planes, stride, downsample))
         self.inplanes = planes * block.expansion
         for i in range(1, blocks):
             layers.append(block(self.inplanes, planes))
         return nn.Sequential(*layers)
-
 
     def forward(self, x):
         # Encoder
@@ -70,11 +75,11 @@ class linknet(nn.Module):
 
         # Decoder with Skip Connections
         d4 = self.decoder4(e4)
-        d4 += e3
+        d4 = d4 + e3
         d3 = self.decoder3(d4)
-        d3 += e2
+        d3 = d3 + e2
         d2 = self.decoder2(d3)
-        d2 += e1
+        d2 = d2 + e1
         d1 = self.decoder1(d2)
 
         # Final Classification
@@ -82,5 +87,4 @@ class linknet(nn.Module):
         f2 = self.finalconvbnrelu2(f1)
         f3 = self.finalconv3(f2)
 
-        return f3 
-
+        return f3
