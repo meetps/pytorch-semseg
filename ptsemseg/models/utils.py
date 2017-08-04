3f2ec6efa2778bfd674e4f613fe745465cbf02e7
Ishrat Badami <badami@vision.rwth-aachen.de>
2017-08-04 16:15:46 +0200

    resolved merge conflict

--- ptsemseg/models/utils.py
+++ ptsemseg/models/utils.py
@@ -9,7 +9,7 @@ class conv2DBatchNorm(nn.Module):
 
         self.cb_unit = nn.Sequential(nn.Conv2d(int(in_channels), int(n_filters), kernel_size=k_size,
                                                padding=padding, stride=stride, bias=bias),
-                                 nn.BatchNorm2d(int(n_filters)),)
+                                     nn.BatchNorm2d(int(n_filters)),)
 
     def forward(self, inputs):
         outputs = self.cb_unit(inputs)
@@ -21,8 +21,8 @@ class deconv2DBatchNorm(nn.Module):
         super(deconv2DBatchNorm, self).__init__()
 
         self.dcb_unit = nn.Sequential(nn.ConvTranspose2d(int(in_channels), int(n_filters), kernel_size=k_size,
-                                               padding=padding, stride=stride, bias=bias),
-                                 nn.BatchNorm2d(int(n_filters)),)
+                                                         padding=padding, stride=stride, bias=bias),
+                                      nn.BatchNorm2d(int(n_filters)),)
 
     def forward(self, inputs):
         outputs = self.dcb_unit(inputs)
@@ -33,10 +33,9 @@ class conv2DBatchNormRelu(nn.Module):
     def __init__(self, in_channels, n_filters, k_size,  stride, padding, bias=True):
         super(conv2DBatchNormRelu, self).__init__()
 
-        self.cbr_unit = nn.Sequential(nn.Conv2d(int(in_channels), int(n_filters), kernel_size=k_size,
-                                                padding=padding, stride=stride, bias=bias),
-                                 nn.BatchNorm2d(int(n_filters)),
-                                 nn.ReLU(inplace=True),)
+        self.cbr_unit = nn.Sequential(nn.Conv2d(int(in_channels), int(n_filters),kernel_size=k_size, padding=padding,
+                                                stride=stride, bias=bias), nn.BatchNorm2d(int(n_filters)),
+                                      nn.ReLU(inplace=True),)
 
     def forward(self, inputs):
         outputs = self.cbr_unit(inputs)
