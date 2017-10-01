a39cb83fad3e8db152fc9c6704b84878eae03263
L0SG <tkdrlf9202@gmail.com>
2017-10-01 14:42:04 +0900

    Fix Unet implementation

--- ptsemseg/models/unet.py
+++ ptsemseg/models/unet.py
@@ -12,28 +12,51 @@ class unet(nn.Module):
         self.feature_scale = feature_scale
 
         filters = [64, 128, 256, 512, 1024]
-        filters = [x / self.feature_scale for x in filters]
+        filters = [int(x / self.feature_scale) for x in filters]
 
-        self.down1 = unetDown(self.in_channels, filters[0], self.is_batchnorm)
-        self.down2 = unetDown(filters[0], filters[1], self.is_batchnorm)
-        self.down3 = unetDown(filters[1], filters[2], self.is_batchnorm)
-        self.down4 = unetDown(filters[2], filters[3], self.is_batchnorm)
+        # downsampling
+        self.conv1 = unetConv2(self.in_channels, filters[0], self.is_batchnorm)
+        self.maxpool1 = nn.MaxPool2d(kernel_size=2)
+
+        self.conv2 = unetConv2(filters[0], filters[1], self.is_batchnorm)
+        self.maxpool2 = nn.MaxPool2d(kernel_size=2)
+
+        self.conv3 = unetConv2(filters[1], filters[2], self.is_batchnorm)
+        self.maxpool3 = nn.MaxPool2d(kernel_size=2)
+
+        self.conv4 = unetConv2(filters[2], filters[3], self.is_batchnorm)
+        self.maxpool4 = nn.MaxPool2d(kernel_size=2)
+
         self.center = unetConv2(filters[3], filters[4], self.is_batchnorm)
-        self.up4 = unetUp(filters[4], filters[3], self.is_deconv)
-        self.up3 = unetUp(filters[3], filters[2], self.is_deconv)
-        self.up2 = unetUp(filters[2], filters[1], self.is_deconv)
-        self.up1 = unetUp(filters[1], filters[0], self.is_deconv)
+
+        # upsampling
+        self.up_concat4 = unetUp(filters[4], filters[3], self.is_deconv)
+        self.up_concat3 = unetUp(filters[3], filters[2], self.is_deconv)
+        self.up_concat2 = unetUp(filters[2], filters[1], self.is_deconv)
+        self.up_concat1 = unetUp(filters[1], filters[0], self.is_deconv)
+
+        # final conv (without any concat)
         self.final = nn.Conv2d(filters[0], n_classes, 1)
 
     def forward(self, inputs):
-        down1 = self.down1(inputs)
-        down2 = self.down2(down1)
-        down3 = self.down3(down2)
-        down4 = self.down4(down3)
-        center = self.center(down4)
-        up4 = self.up4(down4, center)
-        up3 = self.up3(down3, up4)
-        up2 = self.up2(down2, up3)
-        up1 = self.up1(down1, up2)
+        conv1 = self.conv1(inputs)
+        maxpool1 = self.maxpool1(conv1)
 
-        return self.final(up1)
+        conv2 = self.conv2(maxpool1)
+        maxpool2 = self.maxpool2(conv2)
+
+        conv3 = self.conv3(maxpool2)
+        maxpool3 = self.maxpool3(conv3)
+
+        conv4 = self.conv4(maxpool3)
+        maxpool4 = self.maxpool4(conv4)
+
+        center = self.center(maxpool4)
+        up4 = self.up_concat4(conv4, center)
+        up3 = self.up_concat3(conv3, up4)
+        up2 = self.up_concat2(conv2, up3)
+        up1 = self.up_concat1(conv1, up2)
+
+        final = self.final(up1)
+
+        return final
