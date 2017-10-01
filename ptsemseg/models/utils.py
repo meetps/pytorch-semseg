7684d6f11897cc3607f98a3fd9f26be7c8be3aee
L0SG <tkdrlf9202@gmail.com>
2017-10-01 14:42:04 +0900

    Fix Unet implementation

--- ptsemseg/models/utils.py
+++ ptsemseg/models/utils.py
@@ -62,32 +62,20 @@ class unetConv2(nn.Module):
         super(unetConv2, self).__init__()
 
         if is_batchnorm:
-            self.conv1 = nn.Sequential(nn.Conv2d(in_size, out_size, 3, 1, 1),
+            self.conv1 = nn.Sequential(nn.Conv2d(in_size, out_size, 3, 1, 0),
                                        nn.BatchNorm2d(out_size),
                                        nn.ReLU(),)
-            self.conv2 = nn.Sequential(nn.Conv2d(out_size, out_size, 3, 1, 1),
+            self.conv2 = nn.Sequential(nn.Conv2d(out_size, out_size, 3, 1, 0),
                                        nn.BatchNorm2d(out_size),
                                        nn.ReLU(),)
         else:
-            self.conv1 = nn.Sequential(nn.Conv2d(in_size, out_size, 3, 1, 1),
+            self.conv1 = nn.Sequential(nn.Conv2d(in_size, out_size, 3, 1, 0),
                                        nn.ReLU(),)
-            self.conv2 = nn.Sequential(nn.Conv2d(out_size, out_size, 3, 1, 1),
+            self.conv2 = nn.Sequential(nn.Conv2d(out_size, out_size, 3, 1, 0),
                                        nn.ReLU(),)
     def forward(self, inputs):
         outputs = self.conv1(inputs)
         outputs = self.conv2(outputs)
-        return outputs
-
-
-class unetDown(nn.Module):
-    def __init__(self, in_size, out_size, is_batchnorm):
-        super(unetDown, self).__init__()
-        self.conv = unetConv2(in_size, out_size, is_batchnorm)
-        self.down = nn.MaxPool2d(2, 2)
-
-    def forward(self, inputs):
-        outputs = self.conv(inputs)
-        outputs = self.down(outputs)
         return outputs
 
 
@@ -96,14 +84,14 @@ class unetUp(nn.Module):
         super(unetUp, self).__init__()
         self.conv = unetConv2(in_size, out_size, False)
         if is_deconv:
-            self.up = nn.ConvTranspose2d(in_size, out_size, 2)
+            self.up = nn.ConvTranspose2d(in_size, out_size, kernel_size=2, stride=2)
         else:
             self.up = nn.UpsamplingBilinear2d(scale_factor=2)
 
     def forward(self, inputs1, inputs2):
         outputs2 = self.up(inputs2)
         offset = outputs2.size()[2] - inputs1.size()[2]
-        padding = 2*[offset // 2, offset // 2 + 1]
+        padding = 2 * [offset // 2, offset // 2]
         outputs1 = F.pad(inputs1, padding)
         return self.conv(torch.cat([outputs1, outputs2], 1))
 
