ee2c74cd2787195e1db2889cdbd7f71039c05d72
meetshah1995 <meetshah1995@gmail.com>
2017-12-13 01:16:26 +0530

    Add bootstrapped crossentropy loss

--- ptsemseg/loss.py
+++ ptsemseg/loss.py
@@ -17,3 +17,21 @@ def cross_entropy2d(input, target, weight=None, size_average=True):
     if size_average:
         loss /= mask.data.sum()
     return loss
+
+def bootstrapped_cross_entropy2d(input, target, K, weight=None, size_average=True):
+    n, c, h, w = input.size()
+    log_p = F.log_softmax(input)
+    log_p = log_p.transpose(1, 2).transpose(2, 3).contiguous().view(-1, c)
+    log_p = log_p[target.view(n, h, w, 1).repeat(1, 1, 1, c) >= 0]
+    log_p = log_p.view(-1, c)
+
+    loss = F.nll_loss(log_p, target, weight=weight, reduce=False)
+    topk_loss, _ = loss.topk(K)
+    reduced_topk_loss = topk_loss.sum() / K
+
+    return reduced_topk_loss
+
+
+
+
+
