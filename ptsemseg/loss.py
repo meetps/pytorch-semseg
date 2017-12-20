3fd5faf96f4a5c83a2e56f121b508fc7fd53cbae
meetshah1995 <meetshah1995@gmail.com>
2017-12-20 09:28:03 +0530

    Add dim as arg to softmax and flatten tensors for cost computation

--- ptsemseg/loss.py
+++ ptsemseg/loss.py
@@ -6,9 +6,9 @@ import torch.nn.functional as F
 
 def cross_entropy2d(input, target, weight=None, size_average=True):
     n, c, h, w = input.size()
-    log_p = F.log_softmax(input)
+    log_p = F.log_softmax(input, dim=1)
     log_p = log_p.transpose(1, 2).transpose(2, 3).contiguous().view(-1, c)
-    log_p = log_p[target.view(n, h, w, 1).repeat(1, 1, 1, c) >= 0]
+    log_p = log_p[target.view(n * h * w, 1).repeat(1, c) >= 0]
     log_p = log_p.view(-1, c)
 
     mask = target >= 0
@@ -20,9 +20,9 @@ def cross_entropy2d(input, target, weight=None, size_average=True):
 
 def bootstrapped_cross_entropy2d(input, target, K, weight=None, size_average=True):
     n, c, h, w = input.size()
-    log_p = F.log_softmax(input)
+    log_p = F.log_softmax(input, dim=1)
     log_p = log_p.transpose(1, 2).transpose(2, 3).contiguous().view(-1, c)
-    log_p = log_p[target.view(n, h, w, 1).repeat(1, 1, 1, c) >= 0]
+    log_p = log_p[target.view(n * h * w, 1).repeat(1, c) >= 0]
     log_p = log_p.view(-1, c)
 
     loss = F.nll_loss(log_p, target, weight=weight, reduce=False)
