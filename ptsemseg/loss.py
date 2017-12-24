8e30e1bbcda19559645cd378d33115a61ee4e865
meetshah1995 <meetshah1995@gmail.com>
2017-12-24 10:53:10 +0530

    Bootstrap loss from single image and note entire batch

--- ptsemseg/loss.py
+++ ptsemseg/loss.py
@@ -19,21 +19,30 @@ def cross_entropy2d(input, target, weight=None, size_average=True):
     return loss
 
 def bootstrapped_cross_entropy2d(input, target, K, weight=None, size_average=True):
-    n, c, h, w = input.size()
-    log_p = F.log_softmax(input, dim=1)
-    log_p = log_p.transpose(1, 2).transpose(2, 3).contiguous().view(-1, c)
-    log_p = log_p[target.view(n * h * w, 1).repeat(1, c) >= 0]
-    log_p = log_p.view(-1, c)
+    
+    batch_size = input.size()[0]
 
-    mask = target >= 0
-    target = target[mask]
-    loss = F.nll_loss(log_p, target, weight=weight, reduce=False, size_average=False)
-    topk_loss, _ = loss.topk(K)
-    reduced_topk_loss = topk_loss.sum() / K
+    def _bootstrap_xentropy_single(input, target, K, weight=None, size_average=True):
+        n, c, h, w = input.size()
+        log_p = F.log_softmax(input, dim=1)
+        log_p = log_p.transpose(1, 2).transpose(2, 3).contiguous().view(-1, c)
+        log_p = log_p[target.view(n * h * w, 1).repeat(1, c) >= 0]
+        log_p = log_p.view(-1, c)
 
-    return reduced_topk_loss
+        mask = target >= 0
+        target = target[mask]
+        loss = F.nll_loss(log_p, target, weight=weight, reduce=False, size_average=False)
+        topk_loss, _ = loss.topk(K)
+        reduced_topk_loss = topk_loss.sum() / K
 
+        return reduced_topk_loss
 
-
-
-
+    loss = 0.0
+    # Bootstrap from each image not entire batch
+    for i in range(batch_size):
+        loss += _bootstrap_xentropy_single(input=torch.unsqueeze(input[i], 0),
+                                           target=torch.unsqueeze(target[i], 0),
+                                           K=K,
+                                           weight=weight,
+                                           size_average=size_average)
+    return loss / float(batch_size)
