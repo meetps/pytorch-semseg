b59de3485eac2272e6be27d443cbe30f745e242d
meetshah1995 <meetshah1995@gmail.com>
2018-02-11 11:53:44 +0530

    Add paths for NYUv2 and SUNRGBD datasets

--- ptsemseg/loader/__init__.py
+++ ptsemseg/loader/__init__.py
@@ -5,6 +5,9 @@ from ptsemseg.loader.camvid_loader import camvidLoader
 from ptsemseg.loader.ade20k_loader import ADE20KLoader
 from ptsemseg.loader.mit_sceneparsing_benchmark_loader import MITSceneParsingBenchmarkLoader
 from ptsemseg.loader.cityscapes_loader import cityscapesLoader
+from ptsemseg.loader.nyuv2_loader import NYUv2Loader
+from ptsemseg.loader.sunrgbd_loader import SUNRGBDLoader
+
 
 def get_loader(name):
     """get_loader
@@ -17,6 +20,8 @@ def get_loader(name):
         'ade20k': ADE20KLoader,
         'mit_sceneparsing_benchmark': MITSceneParsingBenchmarkLoader,
         'cityscapes': cityscapesLoader,
+        'nyuv2': NYUv2Loader,
+        'sunrgbd': SUNRGBDLoader,
     }[name]
 
 
