8ff983460f73b4baee877eb0eae0b5573d7fdd0e
adam9500370 <adam9500370@yahoo.com.tw>
2018-04-20 19:46:38 +0800

    Fix upsampling for input and target in cross_entropy2d, and add loss func for PSPNet and ICNet

--- ptsemseg/loss.py
+++ ptsemseg/loss.py
@@ -1,28 +1,26 @@
 import torch
 import numpy as np
-import scipy.misc as m
 import torch.nn as nn
 import torch.nn.functional as F
-
-from torch.autograd import Variable
 
 
 def cross_entropy2d(input, target, weight=None, size_average=True):
     n, c, h, w = input.size()
     nt, ht, wt = target.size()
 
-    if h != ht or w != wt: # inconsistent size between input and target
-        lbl = target.data.cpu().numpy()
-        lbl = lbl.astype(float)
-        lbl_resized = np.zeros((n, h, w))
-        for i in range(nt):
-            lbl_resized[i,:,:] = m.imresize(lbl[i,:,:], (h, w), 'nearest', mode='F')
-        lbl_resized = lbl_resized.astype(int)
-        target = Variable(torch.from_numpy(lbl_resized).long().cuda())
+    # Handle inconsistent size between input and target
+    if h > ht and w > wt: # upsample labels
+        target = target.unsequeeze(1)
+        target = F.upsample(target, size=(h, w), mode='nearest')
+        target = target.sequeeze(1)
+    elif h < ht and w < wt: # upsample images
+        input = F.upsample(input, size=(ht, wt), mode='bilinear')
+    elif h != ht and w != wt:
+        raise Exception("Only support upsampling")
 
     log_p = F.log_softmax(input, dim=1)
     log_p = log_p.transpose(1, 2).transpose(2, 3).contiguous().view(-1, c)
-    log_p = log_p[target.view(n * h * w, 1).repeat(1, c) >= 0]
+    log_p = log_p[target.view(-1, 1).repeat(1, c) >= 0]
     log_p = log_p.view(-1, c)
 
     mask = target >= 0
@@ -62,3 +60,17 @@ def bootstrapped_cross_entropy2d(input, target, K, weight=None, size_average=Tru
                                            weight=weight,
                                            size_average=size_average)
     return loss / float(batch_size)
+
+
+def multi_scale_cross_entropy2d(input, target, weight=None, size_average=True, scale_weight=None):
+    # Auxiliary training for PSPNet [1.0, 0.4] and ICNet [1.0, 0.4, 0.16]
+    if scale_weight == None: # scale_weight: torch tensor type
+        n_inp = len(input)
+        scale = 0.4
+        scale_weight = torch.pow(scale * torch.ones(n_inp), torch.arange(n_inp))
+
+    loss = 0.0
+    for i, inp in enumerate(input):
+        loss = loss + scale_weight[i] * cross_entropy2d(input=inp, target=target, weight=weight, size_average=size_average)
+
+    return loss
